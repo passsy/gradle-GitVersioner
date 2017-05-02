0a479d297e2c856c34a38cd88c8f88e6778edf8d
StefMa <StefMaDev@outlook.com>
2017-05-02 07:59:14 +0200

    Make gitVersion and gitVersionName public available

--- gitVersioner/src/main/groovy/com/pascalwelsch/gitversioner/GitVersionerPlugin.groovy
+++ gitVersioner/src/main/groovy/com/pascalwelsch/gitversioner/GitVersionerPlugin.groovy
@@ -8,5 +8,8 @@ class GitVersionerPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def gitVersionExtension = project.extensions.create('gitVersioner', GitVersion)
+        GitVersion gitVersion = new GitVersionGenerator(project, gitVersionExtension).generateVersionName()
+        project.extensions.add("gitVersion", gitVersion)
+        project.extensions.add("gitVersionName", gitVersion.name)
     }
 }
